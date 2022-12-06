6345db9edc41f17e7e2b42e3ab0c516b8c0c8e07
Emmanuel Arhin <dontstop.ea@gmail.com>
2022-12-06 14:06:54 +0000

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/emmanuelarhin/Downloads/django-quiz-app-master/venv/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
