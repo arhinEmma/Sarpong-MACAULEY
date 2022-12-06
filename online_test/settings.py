c7b6dfcb50affd2126c75ce78861cc3c1ccdfc36
Emmanuel Arhin <dontstop.ea@gmail.com>
2022-12-06 14:49:03 +0000

    configure static files

--- online_test/settings.py
+++ online_test/settings.py
@@ -49,9 +49,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    #whitenoice
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'online_test.urls'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
