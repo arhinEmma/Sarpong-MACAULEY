d9b69ff5627b95745ccb74be70722a3da52047bf
Emmanuel Arhin <dontstop.ea@gmail.com>
2022-12-06 14:34:40 +0000

    add allow host

--- online_test/settings.py
+++ online_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q^n4^0*8v2f9%qs$+hg7l0g!-461fja26bzq=cwp)y3u&k6i8&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://sarpong-macauley-production.up.railway.app"]
+ALLOWED_HOSTS = ["sarpong-macauley-production.up.railway.app"]
 
 
 # Application definition
